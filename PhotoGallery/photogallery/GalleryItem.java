6e1504ab7ca0737f7959f03fd09481e7590f1b29
Lis B <eabacus@msn.com>
2017-10-06 13:40:21 -0400

    Add files via upload

---
+++ PhotoGallery/photogallery/GalleryItem.java
@@ -0,0 +1,42 @@
+package com.bignerdranch.android.photogallery;
+
+/**
+ * Created by eabac on 10/6/2017.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+}
+
